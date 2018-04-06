33c7927a684211c54ae6a5e981c57d82940351c8
Bobby Martin <robert.martin.619@gmail.com>
2018-04-06 09:25:30 -0700

    Add third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-function Pizza (topping, size) {
-  this.toppings = topping;
+function Pizza (size) {
+  this.toppings = [0.5, 0.5];
   this.size = size;
 }
 Pizza.prototype.cost = function () {
-  var total = this.toppings + this.size;
+  var cost = 0;
+  this.toppings.map(function(topping) {
+    cost += topping
+  });
+  var total = cost + this.size;
   return total
 };
